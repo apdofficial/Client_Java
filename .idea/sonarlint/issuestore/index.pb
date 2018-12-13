a2dbad7990e8138ada2db36253439e33db5094d1
Andrej Pistek <andrej.pistek@outlook.com>
2018-12-13 09:24:39 +0100

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+I
+src/main/java/Client.java,a/f/afb1b584708e6ac3bfba707cbf2bc88ec58914a5
---
+++ src/main/java/Client.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * client class.
+ *
+ * Just simple Client connecting to the Server and using terminal GUI.
+ *
+ * @author Group 3
+ * @version 0.1
+ */
+public class Client {
+
+    public static void main(String[] args){
+        Socket client = null;
+        BufferedReader inFromServer = null;
+        BufferedWriter outToServer  = null;
+
+
+        try {
+            client = new Socket(InetAddress.getLocalHost(), 49975);
+            inFromServer = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            outToServer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+
+            Scanner input = new Scanner(System.in);
+            System.out.println("Connected to Server on port 49975");
+
+            String strClient,strServer;
+            boolean notRequestedToStop = true;
+            strServer = inFromServer.readLine();
+            System.out.println(strServer);
+
+            while(notRequestedToStop){
+
+                System.out.print("Client : ");
+
+                strClient = input.nextLine();
+                outToServer.write(strClient);
+                outToServer.newLine();
+                outToServer.flush();
+
+                strServer = inFromServer.readLine();
+                System.out.println("Server: " + strServer);
+                System.out.print("\n");
+                if(strServer.equalsIgnoreCase("goodbye")) notRequestedToStop = false;
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                assert inFromServer != null;
+                inFromServer.close();
+                assert outToServer != null;
+                outToServer.close();
+                client.close();
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        
+    }
+}
